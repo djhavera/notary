b7f10ce612f1191b724bb222a011b3a1146cd9f9
David Havera <djhavera@yahoo.com>
2018-10-16 07:55:30 -0400

    Update app.js

--- app.js
+++ app.js
@@ -29,13 +29,23 @@ app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname + '/home.html')));
 
 app.get('/block/:id', async (req, res) => {
-    const blockRes = await blockchain.getBlock(req.params.id);
-    if (blockRes) {
-        res.send(blockRes) // server response 
+    
+    const height = await blockchain.getBlockHeight();
+
+    if (req.params.id <= height) {
+        const blockRes = await blockchain.getBlock(req.params.id);
+        if (blockRes.body.star) {
+            blockRes.body.star.storyDecoded = (new Buffer(blockRes.body.star.story, 'hex')).toString();
+        }
+        if (blockRes) {
+            res.send(blockRes); // server response 
+        } 
     } else {
-        res.status(404).send("The Block was NOT Found")
+        const err = new Error('Block Not Found')
+        err.httpStatusCode = 400
+        err.message = 'Block not found';
+        res.status(err.httpStatusCode).send(err.message);
     }
-});
 
 app.post('/block', async (req, res) => {
     console.log('----------------------------');
